83ed0846e929c289a8bd6be7219f3a3ec20c0f1f
Seth <setha@hey.com>
2022-07-02 15:19:25 -0500

    added swagger documentation to the api

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  email: string;
+
+  @ApiProperty({
+    description: 'The age of the user',
+    minimum: 13,
+  })
+  age: number;
+}
